4d7efffde1eeb7e758a60b38575ed862669ad608
Roman Efremov <roman.efremov236@gmail.com>
2021-11-11 19:08:58 +0300

    Update TravelYandexTrainsPageTests.java

--- lab8/src/test/java/tests/TravelYandexTrainsPageTests.java
+++ lab8/src/test/java/tests/TravelYandexTrainsPageTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -15,6 +16,8 @@ public class TravelYandexTrainsPageTests
     @BeforeMethod
     public void browserSetup()
     {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
